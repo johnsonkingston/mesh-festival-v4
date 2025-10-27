71fc559a9aaeed0d82a5a8cfd92e7a8ea4f9e74a
Johnson / Kingston <post@ivanweiss.net>
2025-10-27 09:08:34 +0100

    Newsletter Form Update

--- static/script/main.js
+++ static/script/main.js
@@ -314,18 +314,18 @@ if(cookie_consent != ""){
     
 
 
-//Analytics
-//<![CDATA[
-    var owa_baseUrl = 'https://johnsonkingston.ch/analytics/';
-    var owa_cmds = owa_cmds || [];
-    owa_cmds.push(['setSiteId', '4ee1809dd8d05ffe44e35fdc857a81b9']);
-    owa_cmds.push(['trackPageView']);
-    owa_cmds.push(['trackClicks']);
+// //Analytics
+// //<![CDATA[
+//     var owa_baseUrl = 'https://johnsonkingston.ch/analytics/';
+//     var owa_cmds = owa_cmds || [];
+//     owa_cmds.push(['setSiteId', '4ee1809dd8d05ffe44e35fdc857a81b9']);
+//     owa_cmds.push(['trackPageView']);
+//     owa_cmds.push(['trackClicks']);
     
-    (function() {
-        var _owa = document.createElement('script'); _owa.type = 'text/javascript'; _owa.async = true;
-        owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl );
-        _owa.src = owa_baseUrl + 'modules/base/dist/owa.tracker.js';
-        var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
-    }());
-//]]>
+//     (function() {
+//         var _owa = document.createElement('script'); _owa.type = 'text/javascript'; _owa.async = true;
+//         owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl );
+//         _owa.src = owa_baseUrl + 'modules/base/dist/owa.tracker.js';
+//         var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
+//     }());
+// //]]>
